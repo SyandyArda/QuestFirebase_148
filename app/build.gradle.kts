5e3f4c43d92768d3822c4e30ae5566c4b76dd37e
Arsyan <syandi.2901@gmail.com>
2026-01-02 06:32:33 -0800

    menambahkan RepositorySiswa.kt di repo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,6 +63,7 @@ dependencies {
 // viewmodel
     implementation(libs.lifecycle.viewmodel.compose)
     implementation(libs.lifecycle.runtime.compose)
+    implementation(libs.firebase.firestore.ktx)
 
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
